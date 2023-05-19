5458cdc20286b1a8823e9fb7c7aac1070d4c7fc9
unknown <sumaiya.rrashid@gmail.comm>
2023-05-19 20:20:34 +0200

    firebase env set

--- src/Component/firebase/firebase.config.js
+++ src/Component/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAZoCxstUyknM3dzpzyjBrriiNg4pP-sLM",
-  authDomain: "marvelous-toys-b4dd5.firebaseapp.com",
-  projectId: "marvelous-toys-b4dd5",
-  storageBucket: "marvelous-toys-b4dd5.appspot.com",
-  messagingSenderId: "885180475586",
-  appId: "1:885180475586:web:95375590be3d1ecce0335b"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
